683a795edabccd33be91d64768f52e68180738b1
JanKadry <jankadry622@gmail.com>
2024-06-21 14:21:12 -0700

    alx

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a random number and checks its properties
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0)); /* Initialize random seed */
+	n = rand() - RAND_MAX / 2; /* Assign random value to n */
+	last_digit = n % 10; /* Get the last digit */
+
+	/* Print the last digit and its properties */
+	printf("Last digit of %d is %d and is ", n, last_digit);
+
+	if (last_digit > 5)
+		printf("greater than 5\n");
+	else if (last_digit == 0)
+		printf("0\n");
+	else
+		printf("less than 6 and not 0\n");
+
+	return (0);
+}
