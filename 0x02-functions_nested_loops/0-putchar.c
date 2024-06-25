3a956173dcbbc917683f937b3e22017394107c53
root <root@Jan.localdomain>
2024-06-26 01:07:23 +0300

    S

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main (void)
+{
+int i ;
+for ( i = 0; i < 8 ; i++)
+{
+    putchar("_putchar"[i]);
+}
+putchar('\n');
+return(0);
+}
