23dde5ed77da58d95d809967448ef7c0ce70587f
JanKadry <jankadry622@gmail.com>
2024-06-26 01:48:58 +0300

    try 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h";
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int i;
+for (i = 0; i < 26; i++)
+{
+_putchar("abcdefghijklmnopqrstuvwxyz"[i]);
+}
+
+return (0);
+}
