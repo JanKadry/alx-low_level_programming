68345925846f8b4437ef90d5a5aed2b8e610966b
JanKadry <jankadry622@gmail.com>
2024-06-26 02:16:20 +0300

    try1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar("abcdefghijklmnopqrstuvwxyz"[i]);
+_putchar('\n');
+}
+
+_putchar('\n');
+}
