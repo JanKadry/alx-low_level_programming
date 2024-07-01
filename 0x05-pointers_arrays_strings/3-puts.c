b19aca92bdbf486b8dded623a260e41a9c0c6a71
JanKadry <jankadry622@gmail.com>
2024-07-02 01:09:57 +0300

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 /**
  * _puts - print a string , followed by a new line to stdout
  * @str: string to print
- * 
+ *
  */
 
 
@@ -10,7 +10,7 @@ void _puts(char *str)
 {
 while (*str != '\0')
 {
-    _putchar(*str++);
+	_putchar(*str++);
 }
 _putchar("\n");
 
