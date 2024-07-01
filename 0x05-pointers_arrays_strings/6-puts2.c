41ab1b1d7a824698afe3933d8dd3808be36064cf
JanKadry <jankadry622@gmail.com>
2024-07-02 01:44:18 +0300

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts2 - reverse a string
+ * @str: input string
+ * return: string in reverse
+ */
+
+void puts2(char *str)
+{
+int length = 0;
+int i = 0;
+char *x = str;
+int j;
+while (*x != '\0')
+{
+x++;
+length++;
+
+}
+i = length - 1;
+for (j = 0; j <= i; j++)
+{
+if (j % 2 == 0)
+{
+_putchar(str[j]);
+}
+}
+
+}
